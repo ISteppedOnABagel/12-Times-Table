a1d9f2ac92f572433da9f4d6f4610610b27a2538
C. Gar <103707181+ISteppedOnABagel@users.noreply.github.com>
2025-12-06 20:28:41 +0000

    Added more style, animations, additional project info

--- script.js
+++ script.js
@@ -1,29 +1,29 @@
-
 // Creates a constant variable (one that doesnt change) and assigns it the value of 12
 const multiplicant = 12;
 
 // Declaring the product, text, and equation variable.
-let head = "The "+ multiplicant +" Times Table<br><br>";
-let product = 0;
+let head = "The " + multiplicant + " Times Table";
+let product = "";
 let equation = "";
-let text = "";
 
-
-
-// For loop that prints the times table from 1 to 12
+// For loop that prints the times table from 1 to 12, it changes the value of i by 1 each time it loops: "i ++"
 for (let i = 1; i <= 12; i++) {
+  // Calculates the product of the multiplicant and i
+  product += "= " + multiplicant * i + "<br>";
 
-// Calculates the product of the multiplicant and i
-product = multiplicant * i;
+  // Creates the equation set up as a string, if i is less than 10 it adds a 0 before i for formatting
 
-// Creates the equation set up as a string
-equation = multiplicant + " x " + i + " = " + product;
-
-// Appends each equation to the text variable with a line break
-text += equation + "<br>";
+  if (i >= 10) {
+    equation += multiplicant + " x " + i + "<br>";
+  }
+  else {
+  equation += multiplicant + " x " + "0" + i + "<br>";
+  }
 }
 
 //Sets the title of the webpage to the times table being displayed
-document.getElementById("Table").innerHTML = head;
-// Outputs the final text to the HTML element with the id of "output"
-document.getElementById("output").innerHTML = text;
+document.getElementById("TableName").innerHTML = head;
+
+// Outputs the equation and product to the webpage
+document.getElementById("equation").innerHTML = equation;
+document.getElementById("product").innerHTML = product;
